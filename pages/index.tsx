4894826c2ef4de6410df089d8f2c87ad46f1ec97
w2h3 <118594933+w2h3@users.noreply.github.com>
2022-12-27 16:39:01 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,19 +3,26 @@ import { useLoadScript } from '@react-google-maps/api'
 import Map from '../components/map';
 import { createClient } from '@vercel/edge-config';
 
-export default function Home() {
+
+export default (req: NextRequest) => {
+  const configItems = await getAll();
+  return NextResponse.json({
+    values: `These are all the values in my Edge Config: ${configItems}`,
+  });
+};
+
+export const config = {
+  runtime: 'experimental-edge',
+};
+
+
+export function Home() {
   const {isLoaded} = useLoadScript({
     googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY ?? '',
     libraries: ['places']
   })
 
 console.log(process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY);
-
-async function getValues() {
-  const secondConfig = createClient(process.env.EDGE_CONFIG);
-  const allValues = await secondConfig.getAll();
-  console.log(allValues);
-}
   
   if(!isLoaded) return 'Loading';
 
